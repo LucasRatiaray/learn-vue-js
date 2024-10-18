d0a402180b35b4069557fcc1a925968a8dae8b19
Lucas <lucas@informatique-m2i.fr>
2024-10-18 17:18:51 +0200

    init vuejs

---
+++ first-starter-vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+
+<button class="border-2 p-2" @click="count++">Click {{ count }}</button>
+
+</template>
+
+<style scoped>
+
+</style>
